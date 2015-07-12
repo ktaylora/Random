b5f82509d900fd0002ed863ee374667bc0d55dfb
Kyle Taylor <ktaylora@Kyles-MacBook-Pro.local>
2015-07-12 12:42:07 -0600

    .

--- sagebrushSubspeciesAnalysis.R
+++ sagebrushSubspeciesAnalysis.R
@@ -1,5 +1,5 @@
 #
-# Big sagebrush subspecies SDMs 
+# Big sagebrush subspecies SDMs
 # A custom implementation of random forest and GLMs, along with spatial aggregation of records
 #
 # Author: Kyle Taylor (kyle.taylor@uwyo.edu)
@@ -22,21 +22,21 @@ spatialPointsToPPP <- function(x,extentMultiplier=1.1){
   require(rgdal)
   require(raster)
   require(spatstat)
-  
+
   e <- extent(x)
-  
-  if(!is.null(extentMultiplier)) { 
+
+  if(!is.null(extentMultiplier)) {
     e@xmin <- e@xmin*extentMultiplier
     e@xmax <- e@xmax+abs(e@xmax*(extentMultiplier-1))
-    e@ymin <- e@ymin-abs(e@ymin*(extentMultiplier-1)) 
+    e@ymin <- e@ymin-abs(e@ymin*(extentMultiplier-1))
     e@ymax <- e@ymax*extentMultiplier
   }
-  
+
   if(class(x) == "SpatialPointsDataFrame"){
     x <- x@coords
     x <- ppp(x=x[,1], y=x[,2], window=owin(xrange=c(e@xmin,e@xmax), yrange=c(e@ymin,e@ymax)))
-  } 
-  
+  }
+
   return(x)
 }
 
@@ -51,7 +51,7 @@ buildTrainingEvaluationSets <- function(p_focal, type="spatially-uniform", debug
   p_focal    <- spTransform(p_focal, CRS(projection(r_template)))
   r_focal    <- crop(r_template, extent(p_focal)*2)
     cat(" -- rasterizing species presence points to a grid that is consistent with our climate data\n")
-      r_focal <- rasterize(p_focal[p_focal$resp ==1,],field='resp', fun=min, r_focal,background=0); 
+      r_focal <- rasterize(p_focal[p_focal$resp ==1,],field='resp', fun=min, r_focal,background=0);
   # generate a sample of potential absence points (i.e., anything that isn't in a presence cell)
   abs_pts <- sampleRandom(r_focal,size=ceiling(20*nrow(p_focal[p_focal$resp==1,])),sp=T) # let's make a large pool of absence records to start with
     abs_pts <- abs_pts[abs_pts$layer==0,]
@@ -65,7 +65,7 @@ buildTrainingEvaluationSets <- function(p_focal, type="spatially-uniform", debug
           abs_pts <- abs_pts[as.vector(!is.na(sp::over(abs_pts,boundaries))),]
             abs_pts <- abs_pts[sample(1:nrow(abs_pts),size=nrow(p_focal[p_focal$resp==1,])),] # downsample to a count consistent with our presence records
   # generate a sampling grid for k-fold crossvalidation from our presence data
-  
+
   ## implement a spatially uniform split?
   if(grepl(type,pattern="uniform")){
     cat(" -- performing spatial uniform sampling\n")
@@ -82,9 +82,9 @@ buildTrainingEvaluationSets <- function(p_focal, type="spatially-uniform", debug
               abline(v=intersection)
     }
     # build a rasterized checkerboard sampling grid
-    samplingGrid <- crop(raster(res=intersection),boundaries)      
+    samplingGrid <- crop(raster(res=intersection),boundaries)
       samplingMatrix <- matrix(c(0,1), nrow=nrow(samplingGrid), ncol=ifelse(!(ncol(samplingGrid)%%2),ncol(samplingGrid)+1,ncol(samplingGrid)), byrow=T)
-        samplingGrid <- setValues(samplingGrid,samplingMatrix[1:nrow(samplingGrid),1:ncol(samplingGrid)])   
+        samplingGrid <- setValues(samplingGrid,samplingMatrix[1:nrow(samplingGrid),1:ncol(samplingGrid)])
     # plot our records space
     if(debug){
       dev.new(height=6,width=8)
@@ -95,8 +95,8 @@ buildTrainingEvaluationSets <- function(p_focal, type="spatially-uniform", debug
     }
     # extract training and evaluation data
     climate_variables <- crop(climate_variables,boundaries,progress='text')
-    cat(" -- extracting climate data for model training")
-    training <- raster::extract(samplingGrid,p_focal[p_focal$resp==1,],sp=T) 
+    cat(" -- extracting climate data for model training\n")
+    training <- raster::extract(samplingGrid,p_focal[p_focal$resp==1,],sp=T)
       training_presence     <- training[training$layer == 1,]
         training_presence@data <- data.frame(resp=rep(1,nrow(training_presence@data)))
           training_presence@data <- cbind(training_presence@data,extract(climate_variables,training_presence))
@@ -111,9 +111,9 @@ buildTrainingEvaluationSets <- function(p_focal, type="spatially-uniform", debug
       evaluation_abs   <- training[as.vector(training[,2]@data == 0),]
         evaluation_abs@data <- data.frame(resp=rep(0,nrow(evaluation_abs@data)))
           evaluation_abs@data <- cbind(evaluation_abs@data,extract(climate_variables,evaluation_abs))
-      
-    training   <- rbind(training_presence,training_abs)@data
-    evaluation <- rbind(evaluation_presence,evaluation_abs)@data
+
+    training   <- rbind(training_presence,training_abs)
+    evaluation <- rbind(evaluation_presence,evaluation_abs)
 
   ## implement the longitudinal strips of Bahn, V., and B. J. McGill. 2013?
   } else if(grepl(type,pattern="longitudinal")){
@@ -124,11 +124,11 @@ buildTrainingEvaluationSets <- function(p_focal, type="spatially-uniform", debug
        training_presence <- presence[presence@coords[,1] < lon_quantiles[1],] # 1st quantile
       training_presence <- rbind(training_presence,
                         presence[presence@coords[,1] > lon_quantiles[2] & # 3rd quantile
-                                 presence@coords[,1] < lon_quantiles[3],]) 
+                                 presence@coords[,1] < lon_quantiles[3],])
     training_abs <- abs_pts[abs_pts@coords[,1] < lon_quantiles[1],] # 1st quantile
       training_abs <- rbind(training_abs,
                             abs_pts[abs_pts@coords[,1] > lon_quantiles[2] & # 3rd quantile
-                                    abs_pts@coords[,1] < lon_quantiles[3],]) 
+                                    abs_pts@coords[,1] < lon_quantiles[3],])
 
     evaluation_presence <- presence[presence@coords[,1] > lon_quantiles[1] & # 2nd quantile
                                    presence@coords[,1] < lon_quantiles[2],]
@@ -149,7 +149,7 @@ buildTrainingEvaluationSets <- function(p_focal, type="spatially-uniform", debug
       training_abs@data <- cbind(training_abs@data,extract(climate_variables,training_abs))
     evaluation_abs@data <- data.frame(resp=rep(0,nrow(evaluation_abs@data)))
       evaluation_abs@data <- cbind(evaluation_abs@data,extract(climate_variables,evaluation_abs))
-      
+
     training   <- rbind(training_presence,training_abs)
     evaluation <- rbind(evaluation_presence,evaluation_abs)
   }
@@ -178,7 +178,7 @@ build_GLM <- function(training,evaluation,formula=NULL,debug=F){
             # tack-on a string of all potential variable interactions
             j<-paste(j,paste(apply(apply(combn(names(training)[2:6],m=2),MARGIN=1,FUN=c),1,FUN=paste,collapse=":"),collapse="+"),sep="+")
               formulas[[length(formulas)+1]] <- formula(paste(names(training)[1],j,sep="~"))
-    # fit a GLM to our formula object and store in a massively inefficient list 
+    # fit a GLM to our formula object and store in a massively inefficient list
     models[[length(models)+1]] <- glm(formula=formulas[[length(formulas)]],family=binomial,data=training); cat(".");
   };cat("\n");
 
@@ -187,7 +187,7 @@ build_GLM <- function(training,evaluation,formula=NULL,debug=F){
     dev.new(height=6,width=8)
     hist(unlist(lapply(models, FUN=AIC)),breaks=70)
   }
-  
+
   # identify which model was the best out of the series by minimizing AIC
   best  <- which(unlist(lapply(models, FUN=AIC)) == min(unlist(lapply(models, FUN=AIC))))
     m_glm <- models[best]
@@ -197,24 +197,24 @@ build_GLM <- function(training,evaluation,formula=NULL,debug=F){
   # find a threshold that optimizes overall model performance against the evaluation dataset and use it to find a cut-off for binary image
   for(c in cutoffs){
     test <- as.numeric(round(as.numeric(predict(m_glm[[1]], newdata=evaluation[,2:ncol(evaluation)],type="resp")),3) > c) == evaluation[,1]
-    glm_eval <- 
+    glm_eval <-
       data.frame(
                   overall=sum(test,na.rm=T)/length(evaluation[,1]),
                   sensitivity=sum(test[1:max(which(evaluation$resp == 1))],na.rm=T)/length(test[1:max(which(evaluation$resp == 1))]),
                   specificity=sum(test[max(which(evaluation$resp == 1)):max(which(evaluation$resp == 0))],na.rm=T)/length(test[max(which(evaluation$resp == 1)):max(which(evaluation$resp == 0))])
-                )  
+                )
     performance <- append(performance,glm_eval[,1])
   }
   # run against our optimal cut-off value and report the value
   c=cutoffs[which(performance == max(performance))];
   test <- as.numeric(round(as.numeric(predict(m_glm[[1]], newdata=evaluation[,2:ncol(evaluation)],type="resp")),3) > c) == evaluation[,1]
-  glm_eval <- 
+  glm_eval <-
       data.frame(
                   overall=sum(test,na.rm=T)/length(evaluation[,1]),
                   sensitivity=sum(test[1:max(which(evaluation$resp == 1))],na.rm=T)/length(test[1:max(which(evaluation$resp == 1))]),
                   specificity=sum(test[max(which(evaluation$resp == 1)):max(which(evaluation$resp == 0))],na.rm=T)/length(test[max(which(evaluation$resp == 1)):max(which(evaluation$resp == 0))]),
                   cutoff=c
-                )  
+                )
   return(list(m_glm,glm_eval))
 }
 
@@ -227,7 +227,7 @@ build_RF <- function(training,evaluation,debug=F){
   require(randomForest)
   m_rf <- randomForest(as.factor(resp)~., data=na.omit(training),importance=T,ntree=2500,do.trace=T)
     test <- as.numeric(as.vector(predict(m_rf,newdata=evaluation[,2:ncol(evaluation)]))) == evaluation[,1]
-  rf_eval <- 
+  rf_eval <-
     data.frame(
                 overall=sum(test,na.rm=T)/length(evaluation[,1]),
                 sensitivity=sum(test[1:max(which(evaluation$resp == 1))],na.rm=T)/length(test[1:max(which(evaluation$resp == 1))]),
@@ -240,7 +240,7 @@ build_RF <- function(training,evaluation,debug=F){
 # MAIN
 #
 
-boundaries <- readOGR(paste(HOME,"/Products/boundaries/",sep=""), "western_north_american_boundaries",verbose=F) 
+boundaries <- readOGR(paste(HOME,"/Products/boundaries/",sep=""), "western_north_american_boundaries",verbose=F)
 # read-in and parse our climate variables
 climate_variables <- list.files(paste(HOME,"/Products/weather/worldclim/30_sec/bioclim",sep=""),pattern="[.]bil$", full.names=T)
   climate_variables <- raster::stack(climate_variables[grepl(climate_variables, pattern="bio_3|bio_4|bio_11|bio_15|bio_18")])
@@ -250,28 +250,60 @@ p_vaseyana     <- readOGR(paste(HOME,"/Products/uw/big_sagebrush_subspp_analysis
 p_wyomingensis <- readOGR(paste(HOME,"/Products/uw/big_sagebrush_subspp_analysis/vectors",sep=""), "wyomingensis_gap_records",verbose=F)
 p_tridentata   <- readOGR(paste(HOME,"/Products/uw/big_sagebrush_subspp_analysis/vectors",sep=""), "tridentata_gap_records",verbose=F)
 
-# build our models
+# build our models for evaluation via longitudinal strips.
 o <- buildTrainingEvaluationSets(p_vaseyana,type="longitudinal",debug=T)
-#   training <- o[[1]]; evaluation <- o[[2]]; rm(o)
-# vaseyana_glm <- build_GLM(training,evaluation)
-# vaseyana_rf  <- build_RF(training,evaluation)
+   training <- o[[1]]@data; evaluation <- o[[2]]@data; rm(o)
 
-# o <- buildTrainingEvaluationSets(p_tridentata,type="longitudinal",debug=T)
-#   training <- o[[1]]; evaluation <- o[[2]]; rm(o)
-# tridentata_glm <- build_GLM(training,evaluation)
-# tridentata_rf  <- build_RF(training,evaluation)
+vaseyana_glm_lon <- build_GLM(training,evaluation)
+vaseyana_rf_lon  <- build_RF(training,evaluation)
 
-# o <- buildTrainingEvaluationSets(p_wyomingensis,type="longitudinal",debug=T)
-#   training <- o[[1]]; evaluation <- o[[2]]; rm(o)
-# wyomingensis_glm <- build_GLM(training,evaluation)
-# wyomingensis_rf  <- build_RF(training,evaluation)
+# now build a real model using a not-crazy sampling approach that will actually produce
+# meaningful predictions
+o <- buildTrainingEvaluationSets(p_vaseyana,type="uniform",debug=F)
+  training <- o[[1]]; evaluation <- o[[2]]; rm(o)
 
-# cat(" -- projecting model rasters\n")
-#   r_glm_vaseyana_current <- predict(climate_variables, vaseyana_glm[[1]][[1]], type='resp', progress='text')
-#   r_rf_vaseyana_current  <- predict(climate_variables, vaseyana_rf[[1]][[1]], type='resp', progress='text')
-# cat(" -- projecting model rasters\n")
-#   r_glm_tridentata_current <- predict(climate_variables, tridentata_glm[[1]][[1]], type='resp', progress='text')
-#   r_rf_tridentata_current  <- predict(climate_variables, tridentata_rf[[1]][[1]], type='resp', progress='text')
+vaseyana_glm_unif <- build_GLM(training,evaluation)
+vaseyana_rf_unif  <- build_RF(training,evaluation)
+
+cat(" -- projecting model rasters\n")
+r_glm_vaseyana_current <- predict(climate_variables, vaseyana_glm_unif[[1]][[1]], type='resp', progress='text')
+r_rf_vaseyana_current  <- 1-predict(climate_variables, vaseyana_rf_unif[[1]], type='prob', progress='text')
+ens_vaseyana_current   <- stackApply(stack(r_glm_vaseyana_current,r_rf_vaseyana_current),fun=mean,indices=1,progress='text')
+
+o <- buildTrainingEvaluationSets(p_tridentata,type="longitudinal",debug=T)
+  training <- o[[1]]@data; evaluation <- o[[2]]@data; rm(o)
+
+tridentata_glm_lon <- build_GLM(training,evaluation)
+tridentata_rf_lon  <- build_RF(training,evaluation)
+
+o <- buildTrainingEvaluationSets(p_tridentata,type="uniform",debug=F)
+  training <- o[[1]]@data; evaluation <- o[[2]]@data; rm(o)
+
+tridentata_glm_unif <- build_GLM(training,evaluation)
+tridentata_rf_unif  <- build_RF(training,evaluation)
+
+cat(" -- projecting model rasters\n")
+r_glm_tridentata_current <- predict(climate_variables, tridentata_glm_unif[[1]][[1]], type='resp', progress='text')
+r_rf_tridentata_current  <- 1-predict(climate_variables, tridentata_rf_unif[[1]], type='prob', progress='text')
+ens_tridentata_current   <- stackApply(stack(r_glm_tridentata_current,r_rf_tridentata_current),fun=mean,indices=1,progress='text')
+
+o <- buildTrainingEvaluationSets(p_wyomingensis,type="longitudinal",debug=T)
+  training <- o[[1]]@data; evaluation <- o[[2]]@data; rm(o)
+
+wyomingensis_glm_lon <- build_GLM(training,evaluation)
+wyomingensis_rf_lon  <- build_RF(training,evaluation)
+
+o <- buildTrainingEvaluationSets(p_wyomingensis,type="uniform",debug=F)
+  training <- o[[1]]@data; evaluation <- o[[2]]@data; rm(o)
+
+wyomingensis_glm_unif <- build_GLM(training,evaluation)
+wyomingensis_rf_unif  <- build_RF(training,evaluation)
+
+cat(" -- projecting model rasters\n")
+r_glm_wyomingensis_current <- predict(climate_variables, wyomingensis_glm_unif[[1]][[1]], type='resp', progress='text')
+r_rf_wyomingensis_current  <- 1-predict(climate_variables, wyomingensis_rf_unif[[1]], type='prob', progress='text')
+ens_wyomingensis_current   <- stackApply(stack(r_glm_wyomingensis_current,r_rf_wyomingensis_current),fun=mean,indices=1,progress='text')
+
 # cat(" -- projecting model rasters\n")
 #   r_glm_wyomingensis_current <- predict(climate_variables, wyomingensis_glm[[1]][[1]], type='resp', progress='text')
 #   r_rf_wyomingensis_current  <- predict(climate_variables, wyomingensis_rf[[1]][[1]], type='resp', progress='text')
@@ -282,9 +314,9 @@ o <- buildTrainingEvaluationSets(p_vaseyana,type="longitudinal",debug=T)
 # par(mar=par()$mar/1.5)
 # mask <- r_glm_tridentata_current >= tridentata_glm[[2]][,4] # mask with binary image
 #   plot(r_glm_tridentata_current*mask,main="ssp tridentata (current climate -- glm)"); plot(boundaries, add=T, border="DarkGrey")
-# mask <- r_glm_wyomingensis_current >= wyomingensis_glm[[2]][,4] 
+# mask <- r_glm_wyomingensis_current >= wyomingensis_glm[[2]][,4]
 #   plot(r_glm_wyomingensis_current*mask,main="ssp wyomingensis (current climate -- glm)"); plot(boundaries, add=T, border="DarkGrey")
-# mask <- r_glm_vaseyana_current >= vaseyana_glm[[2]][,4] 
+# mask <- r_glm_vaseyana_current >= vaseyana_glm[[2]][,4]
 #   plot(r_glm_vaseyana_current*mask,main="ssp vaseyana (current climate -- glm)"); plot(boundaries, add=T, border="DarkGrey")
 
 # dev.new(height=5,width=8.5)
