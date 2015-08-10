fe037db1608e7dc24f5a4a5a6ee66fee57cab2c9
ktaylora <kyle.a.taylor@gmail.com>
2015-08-10 12:13:16 -0600

    house keeping

--- breeding_bird_survey_hierarchical_models/bbs_nMixture_models_workflow_final.R
+++ breeding_bird_survey_hierarchical_models/bbs_nMixture_models_workflow_final.R
@@ -3,10 +3,10 @@
 # Author: Kyle Taylor (kyle.taylor@pljv.org)
 #
 
-HOME <- Sys.getenv("HOME")
+HOME <<- Sys.getenv("HOME")
 
 #
-# calculate site-level landscape metrics for the focal route [parallelized]
+# calculate site-level landscape metrics for the focal route [parallelized -- verified working KT]
 #
 
 processFocalRoute <- function(route=NULL){
@@ -15,57 +15,63 @@ processFocalRoute <- function(route=NULL){
   require(rgdal)
   require(raster)
 
-  names(route@data) <- tolower(names(route@data)) #
+  HOME <- Sys.getenv("HOME")
 
-  landcover <- raster(paste(sep="",HOME,"PLJV/landcover/orig/Final_LC_8bit.tif"))
-  points  <- habitatWorkbench::sampleAroundVertices(s=route,maxDist=2200,n=250) # generate a number of sampling points around each route to derive our site-level landscape metrics
+  names(route@data) <- tolower(names(route@data)) #
+  cat(" -- reading landcover raster\n")
+  landcover <- raster(paste(sep="",HOME,"/PLJV/landcover/orig/Final_LC_8bit.tif"))
+  cat(" -- sampling around vertices for route:",unique(route$rteno),"\n")
+  points  <- habitatWorkbench::sampleAroundVertices(s=route,maxDist=2200,n=350) # generate a number of sampling points around each route to derive our site-level landscape metrics
+  cat(" -- n=",length(points),"points generated\n")
+  cat(" -- generating buffered raster surface around sample points for route:",unique(route$rteno),"\n")
   buffers <- landscapeAnalysis::subsampleSurface(x=landcover,pts=points, width=750) # sample buffers from our source landcover dataset with the points derived from the current route
   # parse our buffers out into focal landcover types
+  cat(" -- reclassifying landcover buffers to binary for route:",unique(route$rteno),"\n")
   buffers_grassland     <- landscapeAnalysis::lReclass(buffers,inValues=c(31,37,39,71,75))
   buffers_agriculture   <- landscapeAnalysis::lReclass(buffers,inValues=c(38,201,202,203,205,206,207,208,209,210,211,212))
   buffers_shrubland     <- landscapeAnalysis::lReclass(buffers,inValues=c(83,85,87,81,82))
   # calculate the requisite landscape metrics for each cover type
+  cat(" -- calculating grassland total area and mean patch area metrics for route:",unique(route$rteno),"\n")
   metrics_grassland <- landscapeAnalysis::lCalculateLandscapeMetrics(buffers_grassland)[[1]] # need: total area and mean patch area
   grassland_total_area <- try(metricsListToVector(metrics_grassland,'total.area'))
     if(class(grassland_total_area)=="try-error"){
       print(str(metrics_grassland))
       grassland_total_area <- 0
     } else {
-      grassland_total_area <- ifelse(is.null(grassland_total_area),0,grassland_total_area)
-        grassland_total_area[is.na(grassland_total_area)] <- 0
-          grassland_total_area <- mean(grassland_total_area)
+      grassland_total_area <- ifelse(is.null(grassland_total_area),0,mean(grassland_total_area,na.rm=T))
+        if(is.na(grassland_total_area)) grassland_total_area <- 0
     }
   grassland_mean_patch_area <- try(metricsListToVector(metrics_grassland,'mean.patch.area'))
     if(class(grassland_mean_patch_area)=="try-error"){
       print(str(metrics_grassland))
       grassland_mean_patch_area <- 0
     } else {
-      grassland_mean_patch_area <- ifelse(is.null(grassland_mean_patch_area),0,grassland_mean_patch_area)
-        grassland_mean_patch_area[is.na(grassland_mean_patch_area)] <- 0
-          grassland_mean_patch_area <- mean(grassland_mean_patch_area)
+      grassland_mean_patch_area <- ifelse(is.null(grassland_mean_patch_area),0,mean(grassland_mean_patch_area,na.rm=T))
+        if(is.na(grassland_mean_patch_area)) grassland_mean_patch_area <- 0
     }
+  cat(" -- calculating agriculture total area for route:",unique(route$rteno),"\n")
   metrics_agriculture   <- landscapeAnalysis::lCalculateLandscapeMetrics(buffers_agriculture) # need: total area
   agriculture_total_area <- try(metricsListToVector(metrics_agriculture,'total.area'))
     if(class(agriculture_total_area)=="try-error"){
       print(str(metrics_agriculture))
       agriculture_total_area <- 0
     } else {
-      agriculture_total_area <- ifelse(is.null(agriculture_total_area),0,agriculture_total_area)
-        agriculture_total_area[is.na(agriculture_total_area)] <- 0
-          agriculture_total_area <- mean(agriculture_total_area)
+      agriculture_total_area <- ifelse(is.null(agriculture_total_area),0,mean(agriculture_total_area,na.rm=T))
+        if(is.na(agriculture_total_area)) agriculture_total_area <- 0
     }
+  cat(" -- calculating shrubland total area for route:",unique(route$rteno),"\n")
   metrics_shrubland     <- landscapeAnalysis::lCalculateLandscapeMetrics(buffers_shrubland) # need: total area
   shrubland_total_area <- try(metricsListToVector(metrics_shrubland,'total.area'))
     if(class(shrubland_total_area)=="try-error"){
       print(str(metrics_shrubland))
       shrubland_total_area <- 0
     } else {
-      shrubland_total_area <- ifelse(is.null(shrubland_total_area),0,shrubland_total_area)
-        shrubland_total_area[is.na(shrubland_total_area)] <- 0
-          shrubland_total_area <- mean(shrubland_total_area)
+      shrubland_total_area <- ifelse(is.null(shrubland_total_area),0,mean(shrubland_total_area,na.rm=T))
+        if(is.na(shrubland_total_area)) shrubland_total_area <- 0
     }
   # write to output table
-  return(data.frame(route=route$rteno,grass_total_area=grassland_total_area,grass_mean_patch_area=grassland_mean_patch_area,
+  cat(" -- returning data.frame from thread for route:",unique(route$rteno),"\n")
+  return(data.frame(route=unique(route$rteno),grass_total_area=grassland_total_area,grass_mean_patch_area=grassland_mean_patch_area,
     ag_total_area=agriculture_total_area,shrub_total_area=shrubland_total_area))
 }
 
@@ -76,7 +82,7 @@ processFocalRoute <- function(route=NULL){
 require(rgdal)
 require(raster)
 require(habitatWorkbench)
-require(parallel); cl <- makeCluster(getOption("cl.cores", 7),outfile='outfile.log')
+require(parallel); cl <- makeCluster(getOption("cl.cores", parallel::detectCores()-1),outfile='outfile.log')
 
 # read-in our local (study area) routes
 # t_routes_bcr1819 <<- read.csv(paste(sep="",HOME,"PLJV/species_data/bbs_data/bbs_routes_bcr1819.csv")) # pre-define the route numbers needed in an external GIS
@@ -86,18 +92,19 @@ require(parallel); cl <- makeCluster(getOption("cl.cores", 7),outfile='outfile.l
 # data(bbsRoutes); s_bbsRoutes <- s_bbsRoutes[s_bbsRoutes$RTENO %in% t_routes_bcr1819$RTENO,] # RTENO : [STATE(2-digit)][ROUTE(3-digit)]
 
 # ensure we have adequate land cover data for our bbs routes
-landcover <- raster(paste(sep="",HOME,"PLJV/landcover/orig/Final_LC_8bit.tif"))
-  s_bbsRoutes <- spTransform(readOGR(paste(sep="",HOME,"PLJV/species_data/bbs_data/"),"Breeding_Bird_Surveys_1998to2013",verbose=F),CRS(projection(landcover)))
-    landcover <- extract(landcover,as(spTransform(s_bbsRoutes,CRS(projection(landcover))),'SpatialPointsDataFrame'),sp=T)
+lc <- raster(paste(sep="",HOME,"/PLJV/landcover/orig/Final_LC_8bit.tif"))
+ s_bbsRoutes <- spTransform(readOGR(paste(sep="",HOME,"/PLJV/species_data/bbs_data/"),"Breeding_Bird_Surveys_1998to2013",verbose=F),CRS(projection(lc)))
+    lc <- extract(lc,as(spTransform(s_bbsRoutes,CRS(projection(lc))),'SpatialPointsDataFrame'),sp=T)
 
- s_bbsRoutes <- s_bbsRoutes[s_bbsRoutes$rteno %in% unique(landcover[!is.na(landcover$Final_LC_8bit),]$rteno),] # make sure that our route data has landcover data available
-   s_bbsRoutes <- s_bbsRoutes[s_bbsRoutes$rteno %in% unique(landcover[landcover$Final_LC_8bit != 0,]$rteno),]
+ s_bbsRoutes <- s_bbsRoutes[s_bbsRoutes$rteno %in% unique(lc[!is.na(lc$Final_LC_8bit),]$rteno),] # make sure that our route data has landcover data available
+   s_bbsRoutes <- s_bbsRoutes[s_bbsRoutes$rteno %in% unique(lc[lc$Final_LC_8bit != 0,]$rteno),]
 
+ s_bbsRoutes_sampling <- s_bbsRoutes[!duplicated(s_bbsRoutes$rteno),] # there are duplicate points for each year.  For sampling LMs, let's just trim this to a single year.
 # split our routes so that they can be processed in parallel on a multi-core machine and then calculate some site level statistics for landcover at each route
-s_bbsRoutes <- split(s_bbsRoutes,f=1:nrow(s_bbsRoutes))
+s_bbsRoutes_sampling <- split(s_bbsRoutes_sampling,f=as.factor(s_bbsRoutes_sampling$rteno))
 if(!file.exists("site_level_parameters.csv")){
   cat(" -- sampling and processing BBS routes\n");
-    out  <- parLapply(cl=cl,fun=processFocalRoute,X=s_bbsRoutes)
+    out  <- parLapply(cl=cl,fun=processFocalRoute,X=s_bbsRoutes_sampling)
       out <- do.call(rbind,out) # bind our list into a single data.frame
         write.csv(out,"site_level_parameters.csv",row.names=F)
 } else {
@@ -107,55 +114,58 @@ if(!file.exists("site_level_parameters.csv")){
 ## CALCULATE OUR ISOLATION METRICS AT 1650 AND 30000 METER BUFFER DISTANCES
 
 if(!grepl(names(out),pattern="isolation_1650m")){
-      landcover <- raster(paste(sep="",HOME,"PLJV/landcover/orig/Final_LC_8bit.tif"))
-    s_bbsRoutes <-lapply(s_bbsRoutes,spTransform,CRSobj=CRS(projection(landcover)))
-      centroids <- parLapply(cl=cl,s_bbsRoutes,fun=getBbsRouteCoords,centroid=T)     # calculate the centroid of each route
-  buffers_3.3km <- parLapply(cl=cl,X=centroids,fun=rgeos::gBuffer,width=3300/2)      # calculate our 3.3 km regions
-    m <- mcmapply(buffers_3.3km, FUN=raster::crop, MoreArgs=list(x=landcover))
-      buffers_3.3km <- mcmapply(FUN=raster::mask, x=m, mask=buffers_3.3km); rm(m);
-        buffers_3.3km <- lReclass(buffers_3.3km,inValues=c(31,37,39,71,75))
-          buffers_3.3km <- parLapply(cl=cl,buffers_3.3km,fun=landscapeAnalysis::rasterToPolygons)
+    s_bbsRoutes_sampling <-lapply(s_bbsRoutes_sampling,spTransform,CRSobj=CRS(projection(lc)))
+      centroids <- parLapply(cl=cl,s_bbsRoutes_sampling,fun=getBbsRouteCoords,centroid=T)     # calculate the centroid of each route
+  buffers_1650m <- parLapply(cl=cl,X=centroids,fun=rgeos::gBuffer,width=3300/2)      # calculate our 3.3 km regions
+    m <- mcmapply(buffers_1650m, FUN=raster::crop, MoreArgs=list(x=lc))
+      buffers_1650m <- mcmapply(FUN=raster::mask, x=m, mask=buffers_1650m); rm(m);
+        buffers_1650m <- lReclass(buffers_1650m,inValues=c(31,37,39,71,75))
+          buffers_1650m <- parLapply(cl=cl,buffers_1650m,fun=landscapeAnalysis::rasterToPolygons)
             # check for NA values
-            na_values<-as.vector(unlist(lapply(X=buffers_3.3km,FUN=is.na)))
+            na_values<-as.vector(unlist(lapply(X=buffers_1650m,FUN=is.na)))
             if(sum(na_values)>0){
-              buffers_3.3km[na_values] <- buffers_3.3km[which(!na_values)[1]] # overwrite our NA values with something valid
-              buffers_3.3km<-lapply(X=buffers_3.3km,FUN=getSpPPolygonsLabptSlots)
+              buffers_1650m[na_values] <- buffers_1650m[which(!na_values)[1]] # overwrite our NA values with something valid
+              buffers_1650m<-lapply(X=buffers_1650m,FUN=getSpPPolygonsLabptSlots)
 
             } else {
-              buffers_3.3km<-lapply(X=buffers_3.3km,FUN=getSpPPolygonsLabptSlots)
+              buffers_1650m<-lapply(X=buffers_1650m,FUN=getSpPPolygonsLabptSlots)
             }
             # do our NN assessment
             d <- function(x,na.rm=F){ o<-try(FNN::knn.dist(x,k=1)); if(class(o) != "try-error") { x <- o; } else { x[[i]] <- NA }; return(x)}
-            buffers_3.3km <- lapply(buffers_3.3km,FUN=d);rm(d);
-              buffers_3.3km <- lapply(buffers_3.3km, FUN=mean)
-                buffers_3.3km[na_values] <- NA # restore our NA values
-                  buffers_3.3km
-                    out$isolation_3.3km<-as.vector(unlist(buffers_3.3km))
+            buffers_1650m <- lapply(buffers_1650m,FUN=d);rm(d);
+              buffers_1650m <- lapply(buffers_1650m, FUN=mean)
+                buffers_1650m[na_values] <- NA # restore our NA values
+                  buffers_1650m
+                    out$isolation_1650m<-as.vector(unlist(buffers_1650m))
 }
 
+centroids <- parLapply(cl=cl,s_bbsRoutes_sampling,fun=getBbsRouteCoords,centroid=T)
+  buffers <- subsampleSurface(pts=do.call(rbind,centroids),width=3300/2)
+    buffers <- lReclass(buffers,inValues=c(31,37,39,71,75))
+out <- lapply(buffers,FUN=calcPatchIsolation, parallel=T)
+
 if(!grepl(names(out),pattern="isolation_30000m")){
-      landcover <- raster(paste(sep="",HOME,"PLJV/landcover/orig/Final_LC_8bit.tif"))
-    s_bbsRoutes <-lapply(s_bbsRoutes,spTransform,CRSobj=CRS(projection(landcover)))
+    s_bbsRoutes_sampling <-lapply(s_bbsRoutes_sampling,spTransform,CRSobj=CRS(projection(lc)))
       centroids <- parLapply(cl=cl,s_bbsRoutes,fun=getBbsRouteCoords,centroid=T) # calculate the centroid of each route
-  buffers_30km <- parLapply(cl=cl,X=centroids,fun=rgeos::gBuffer,width=30000)      # calculate our 3.3 km regions
-    m <- mcmapply(buffers_30km, FUN=raster::crop, MoreArgs=list(x=landcover))
-      buffers_30km <- mcmapply(FUN=raster::mask, x=m, mask=buffers_30km); rm(m);
-        buffers_30km <- lReclass(buffers_30km,inValues=c(31,37,39,71,75))
-          buffers_30km <- parLapply(cl=cl,buffers_30km,fun=landscapeAnalysis::rasterToPolygons)
+  buffers_30000m <- parLapply(cl=cl,X=centroids,fun=rgeos::gBuffer,width=30000)      # calculate our 3.3 km regions
+    m <- mcmapply(buffers_30000m, FUN=raster::crop, MoreArgs=list(x=lc))
+      buffers_30000m <- mcmapply(FUN=raster::mask, x=m, mask=buffers_30000m); rm(m);
+        buffers_30000m <- lReclass(buffers_30000m,inValues=c(31,37,39,71,75))
+          buffers_30000m <- parLapply(cl=cl,buffers_30000m,fun=landscapeAnalysis::rasterToPolygons)
             # check for NA values
-            na_values<-as.vector(unlist(lapply(X=buffers_30km,FUN=is.na)))
+            na_values<-as.vector(unlist(lapply(X=buffers_30000m,FUN=is.na)))
             if(sum(na_values)>0){
-              buffers_30km[na_values] <- buffers_30km[which(!na_values)[1]] # overwrite our NA values with something valid
-              buffers_30km<-lapply(X=buffers_30km,FUN=getSpPPolygonsLabptSlots)
+              buffers_30000m[na_values] <- buffers_30000m[which(!na_values)[1]] # overwrite our NA values with something valid
+              buffers_30000m<-lapply(X=buffers_30000m,FUN=getSpPPolygonsLabptSlots)
             } else {
-              buffers_30km<-lapply(X=buffers_30km,FUN=getSpPPolygonsLabptSlots)
+              buffers_30000m<-lapply(X=buffers_30000m,FUN=getSpPPolygonsLabptSlots)
             }
             # do our NN assessment
             d <- function(x,na.rm=F){ o<-try(FNN::knn.dist(x,k=1)); if(class(o) != "try-error") { x <- o; } else { x[[i]] <- NA }; return(x)}
-            buffers_30km <- lapply(buffers_30km,FUN=d);rm(d);
-              buffers_30km <- lapply(buffers_30km, FUN=mean)
-                buffers_30km[na_values] <- NA # restore our NA values
-                    out$isolation_30km<-as.vector(unlist(buffers_30km))
+            buffers_30000m <- lapply(buffers_30000m,FUN=d);rm(d);
+              buffers_30000m <- lapply(buffers_30000m, FUN=mean)
+                buffers_30000m[na_values] <- NA # restore our NA values
+                    out$isolation_30000m<-as.vector(unlist(buffers_30000m))
 }
 
 write.csv(out,"site_level_parameters.csv",row.names=F)
