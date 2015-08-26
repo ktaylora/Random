8feabf25b15d02e0dd1340c790853d7bfb4ec426
Kyle Taylor <ktaylora@users.noreply.github.com>
2015-08-26 11:13:33 -0600

    Update bbs_nMixture_models_workflow_final.R

--- breeding_bird_survey_hierarchical_models/bbs_nMixture_models_workflow_final.R
+++ breeding_bird_survey_hierarchical_models/bbs_nMixture_models_workflow_final.R
@@ -304,6 +304,7 @@ ratioCU_to_BBS_Route <- (4.253/((3.1415926*(39.4289/50/2)^2)*50))
 m_buow <- function(x,type="normalized"){
   x <- rowSums(x[,c('gr_tAr','gr_patAr','ag_tAr','sh_tAr','is_1650','is_30km')]*as.numeric(0.000717,0.001151,0.001088,0.001379,-0.000491,-0.004840))
     x <- as.vector(x+1.997769) # intercept
+      x[x<0] <- 0 # don't return irrational counts
     # normalize to regional counts?
     if(grepl(tolower(type),pattern="normal")){
       x <- x/mean(x) # normalize to the mean count
@@ -318,6 +319,7 @@ m_buow <- function(x,type="normalized"){
 m_feha<- function(x,type="normalized"){
   x <- rowSums(x[,c('gr_tAr','gr_patAr','ag_tAr','sh_tAr','is_1650','is_30km')]*as.numeric(-0.001144,0.000341,-0.002519,-0.001254,-0.000543,0.005354))
     x <- as.vector(x+1.164134) # intercept
+      x[x<0] <- 0 # don't return irrational counts
     # normalize to regional counts?
     if(grepl(tolower(type),pattern="normal")){
       x <- x/mean(x) # normalize to the mean count
@@ -332,6 +334,7 @@ m_feha<- function(x,type="normalized"){
 m_lbcu<- function(x,type="normalized"){
   x <- rowSums(x[,c('gr_tAr','gr_patAr','ag_tAr','sh_tAr','is_1650','is_30km')]*as.numeric(2.81e-03,-4.58e-03,3.21e-04,6.46e-05,3.31e-03,-4.07e-04))
     x <- as.vector(x+7.36e-02) # intercept
+      x[x<0] <- 0 # don't return irrational counts
     # normalize to regional counts?
     if(grepl(tolower(type),pattern="normal")){
       x <- x/mean(x) # normalize to the mean count
