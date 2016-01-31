3f32a948485f7ccf81ca4cadbbcd0b0590ce344d
Kyle Taylor <ktaylora@default.local>
2016-01-31 13:22:52 -0700

    house keeping

--- lek_resource_selection/WORKFLOW_02_Upscale_Climate_Data.R
+++ lek_resource_selection/WORKFLOW_02_Upscale_Climate_Data.R
@@ -4,10 +4,63 @@
 # Author: Kyle Taylor (kyle.taylor@pljv.org) [2016]
 #
 
-# load our default libraries
-require(raster)
-require(rgdal)
-require(parallel)
+#
+# LOAD OUR DEFAULT LIBRARIES
+#
+
+require(raster)   # for manipulating georasters
+require(rgdal)    # for working with ESRI Shapefiles
+require(parallel) # for parallelizing our workflow across multiple CPU cores
+
+#
+# DEFINE LOCAL FUNCTIONS FOR THE WORKFLOW
+#
+
+#
+# fetchClimateData()
+# Local function that downloads a zipfile containing the Reihfeldt climate variable of
+# interest from the USFS FTP server.  Does some redundant checking to make sure we aren't
+# duplicating downloads if the file already exists in the CWD.  Make this parallelized by
+# including the necessary 'R' libraries inside the function call.
+#
+
+fetchClimateData <- function(var=NULL,dest="."){
+  require(raster)
+  # is the focal variable fetchable from the Reihfeldt dataset?
+  fetchable <- c("map","mat_tenths","ffp") # hardcoded values from USFS FTP server
+  if(!sum(grepl(var,fetchable))){
+    warning(paste(dest,"/",fetchable[var],".zip isn't a known Reihfeldt variable. Skipping.",sep=""))
+    return(NULL)
+  } else {
+    var <- which(grepl(var,fetchable))
+    if(file.exists(paste(dest,"/",fetchable[var],".zip",sep=""))){
+      warning(paste(dest,"/",fetchable[var],".zip already exists. Skipping.",sep=""))
+    } else {
+      cat(" -- downloading.\n")
+      download.file(url=paste("http://forest.moscowfsl.wsu.edu/climate/current/allNA/derivedGrids/",fetchable[var],".zip",sep=""),
+                    destfile=paste(dest,"/",fetchable[var],".zip",sep=""))
+    }
+  }
+  # Decompress and process the raster file, returning to user as a raster object
+  if(!file.exists(paste(fetchable[var],"asc",sep="."))){
+    cat(" -- decompressing.\n")
+    utils::unzip(paste(dest,"/",fetchable[var],".zip",sep=""),exdir=dest)
+    cat(" -- processing.\n")
+    file.rename(paste(dest,"/",fetchable[var],".txt",sep=""),paste(dest,"/",fetchable[var],".asc",sep=""))
+  } else {
+    warning(paste(dest,"/",fetchable[var],".asc already exists. Did nothing",sep=""))
+  }
+  cat(" -- done.\n")
+  var <- raster(paste(dest,"/",fetchable[var],".asc",sep=""),crs=CRS(projection("+init=epsg:4326"))) # read raster from file with a forced EPSG resolution
+  return(var)
+}
+
+#
+# MAIN WORKFLOW
+#
+
+# prepare our local 'cluster'
+cl <- parallel::makeCluster(4) # use 4 CPU cores
 
 # by default, let's assume the user passed us a directory path at runtime
 argv <- commandArgs(trailingOnly=T)
@@ -15,12 +68,11 @@ path <- if(file.exists(argv),argv,".")
 
 # read-in our source raster data from a directory path provided by the user
 elevation  <- raster(paste(path,"elevation.tif",sep="//"))
-cliRasters <- ifelse(file.exists(argv[1]), argv[1], file.choose())
-  cliRasters <- list.files(cliRasters, pattern=paste(c("temp.img$","precip.img$","yada.img$"),collapse="|")) # filter files by expected names
-    cliRasters <- lapply(as.list(cliRasters),fun=raster)
+cliRasters <- ifelse(file.exists(path), path, file.choose())
+  cliRasters <- parLapply(as.list(c("mat_tenths","map","ffp")),FUN=fetchClimateData, dest=cliRasters)
 
 # crop our rasters to the extent of the study area
-studyAreaExtent <- readOGR(path,"studyAreaExtent",verbose=F)
+studyAreaExtent <- readOGR(path,"Study_area",verbose=F)
   studyAreaExtent <- spTransform(studyAreaExtent,CRS(projection(elevation))) # re-project our shapefile to the CRS of our elevation DEM
 
 cliRasters <- lapply(cliRasters,FUN=crop,studyAreaExtent)
