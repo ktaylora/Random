9956d5b35969c1f2d1afb7a6b736756e93df93e8
Kyle Taylor <ktaylora@users.noreply.github.com>
2016-02-28 11:10:42 -0700

    Update sockets.py
    house keeping

--- sockets.py
+++ sockets.py
@@ -1,6 +1,6 @@
-'''
+"""
 A simple interface for network handshaking and socket handling for client/server tasks
-'''
+"""
 
 import socket
 import threading
@@ -9,13 +9,14 @@ import zlib
 
 from socket import error as socket_error
 
+
 class HandShake():
-    ''' 
-    A roll-your-own implementation of handshaking loosely based on WebSockets.  HandShake is a parent class 
-    containing methods and attributes useful for negotiating common client/server tasks (e.g., file send/recieve, 
-    searching, etc...).
-    '''
-    def __init__(self,*kargs):
+    def __init__(self, *args):
+        """
+        A roll-your-own implementation of handshaking loosely based on WebSockets.  HandShake is a parent class
+        containing methods and attributes useful for negotiating common client/server tasks (e.g., file send/recv,
+        searching, etc...).
+        """
         self.HOST = ''
         self.CONNECTION = ''
         self.SEC_KEY = ''
@@ -23,61 +24,122 @@ class HandShake():
         self.SEC_VERSION = ''
         self.MESSAGE = ''
 
+
 class ClientConnection(threading.Thread):
-    def __init__(self,*kargs):
+    def __init__(self, *args):
+        """
+        parent class providing exposure to handshaking and threading for client-handling tasks
+        :param args:
+        :return:
+        """
         self.h = HandShake()
         threading.Thread.__init__(self)
         try:
-            self.socket = kargs[0]
+            self.socket = args[0]
             self.connection, self.address = self.socket.accept()
             print " -- client connect from", self.address[0]
         except socket_error as sError:
             raise sError
-    def echo(self):
+
+    def run(self):
+        """
+        default run method provides a simple threaded echo server.  Default method should be overwritten by derived
+        child classes to provide more complex functionality
+        :return NULL:
+        """
         while True:
             try:
                 data = self.connection.recv(256)
             except socket_error as sError:
                 raise sError
-            if data :
+            if data:
                 self.connection.send(data)
-    def run(self):
-        self.echo()
 
-class GetFile(ClientConnection):
-    def __init__(self,*kargs):
-        ClientConnection.__init__(self,kargs[0])
-    def handshake(self):
-        print "hi"
 
-class SendFile(ClientConnection):
-    def __init__(self,*kargs):
-        ClientConnection.__init__(self,kargs[0])
-    def handshake(self):
-        print "hi"
+class ServerConnection(threading.Thread):
+    def __init__(self, *args):
+        """
+        parent class providing exposure to handshaking and threading for client-handling tasks
+        :param args:
+        :return:
+        """
+        self.h = HandShake()
+        threading.Thread.__init__(self)
 
-class LocateFile(ClientConnection):
-    def __init__(self,*kargs):
-        ClientConnection.__init__(self,kargs[0])
 
-class Authenticate(ClientConnection):
-    def __init__(self,*kargs):
-        ClientConnection.__init__(self,kargs[0])
+class SGetFile(ClientConnection):
+    def __init__(self, *args):
+        """
+        GetFile parent class serves/requests files to a connected client upon request
+        :param args:
+        :return:
+        """
+        ClientConnection.__init__(self, args[0])
+
+
+class CGetFile(ServerConnection):
+    def __init__(self, *args):
+        """
+        GetFile parent class serves/requests files to a connected client upon request
+        :param args:
+        :return:
+        """
+        ServerConnection.__init__(self, args[0])
+
+class SLocateFile(ClientConnection):
+    def __init__(self, *args):
+        """
+        client-related LocateFile interface that accepts a filename and attempts to locate a file on the server-side,
+        using UNIX tools.
+        :param args:
+        :return:
+        """
+        ClientConnection.__init__(self, args[0])
+
+
+class SAuthenticate(ClientConnection):
+    def __init__(self, *args):
+        """
+        client-related Authenicate interface that handles a user request for authentication
+        :param args:
+        :return:
+        """
+        ClientConnection.__init__(self, args[0])
+
+class CAuthenticate(ServerConnection):
+    def __init__(self, *args):
+        """
+        client-related Authenicate interface that handles a user request for authentication
+        :param args:
+        :return:
+        """
+        ServerConnection.__init__(self, args[0])
 
 class Server:
-    def __init__(self,*kargs):
+    def __init__(self, *args):
+        """
+
+        :param args:
+        :return:
+        """
         self.socket = socket.socket()
-        if(len(kargs)>0):
-            self.port = kargs[0]
-            self.maxConnect = kargs[1] # sets the number of queued clients on our single-thread stack while we work with our active connection
+        if len(args) > 0:
+            self.port = args[0]
+            self.maxConnect = args[
+                1]  # sets the number of queued clients on our single-thread stack while we work with our active connection
         else:
             self.port = 65000
             self.maxConnect = 5
         try:
-            self.socket.bind(('',self.port))  # bind to localhost on port N
+            self.socket.bind(('', self.port))  # bind to localhost on port N
         except socket_error as sError:
             raise sError
+
     def listen(self):
+        """
+
+        :rtype: object
+        """
         try:
             self.socket.listen(self.maxConnect)
         except socket_error as sError:
@@ -85,9 +147,16 @@ class Server:
         self.client_list = []
         while True:
             self.client_list.append(ClientConnection(self.socket).start())
+
     def close(self):
         self.socket.close()
 
-if __name__ == "__main__":
-    s=Server(12345,10)
-    s.listen()
+
+if __name__ == "_main__":
+    try:
+        s = Server(12345, 10)
+        s.listen()
+    except KeyboardInterrupt as e:
+        s.close()
+    except Exception as e:
+        raise e
