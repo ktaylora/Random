6e7599d0ac57aaf3a79c92fe0bdedf9575750f64
ktaylora <kyle.a.taylor@gmail.com>
2016-03-07 14:38:29 -0700

    house keeping

--- calculate_aquifer_saturated_thickness/WORKFLOW_03_Finalize_Aquifer_Data.R
+++ calculate_aquifer_saturated_thickness/WORKFLOW_03_Finalize_Aquifer_Data.R
@@ -8,6 +8,7 @@ require(parallel)
 
 cl <- makeCluster(4)
 argv <- commandArgs(trailingOnly=T)
+  if(length(argv)<1) argv <- "." # by default, let's assume that the user is working in the CD. 
 
 if(!file.exists(argv[1])) stop("usage: R < script [directory with rasters and maskfile]")
 
@@ -19,9 +20,7 @@ r <- list.files(argv[1],pattern="tif$")
 s <- spTransform(s,CRS(projection(r[[1]])))
   r <- parLapply(cl,r,mask,mask=s)
 
-satThick_08_12 <- stackApply(stack(r[grepl(unlist(lapply(r,names)),pattern="2008|2009|2010|2011|2012")]),fun=mean,indices=1)
-   satThick_13 <- r[grepl(unlist(lapply(r,names)),pattern="2013")][[1]]
+satThick_10_14 <- stackApply(stack(r[grepl(unlist(lapply(r,names)),pattern="2010|2011|2012|2013|2014")]),fun=mean,indices=1)
 
 # write our output to disk
-writeRaster(satThick_08_12,"satThick_08_12.tif",overwrite=T)
-writeRaster(satThick_13,"satThick_13.tif",overwrite=T)
+writeRaster(satThick_10_14,"satThick_10_14.tif",overwrite=T)
