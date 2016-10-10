fdf39157b2ad92127f66aa6c17ca24824156e88d
Kyle Taylor <kyle.a.taylor@gmail.com>
2016-10-10 16:28:05 -0600

    quick landfire post-process calculator

---
+++ gdal_post-process_landfire.sh
@@ -0,0 +1,14 @@
+# calculate grass height (units are meters / 100)
+# percent cover units are 0.N * 100
+in=`echo "$1" | awk '{print tolower($0)}'`
+if echo $in | grep -q "evh"; then
+  rm -rf grass_height* shrub_height* tree_height*
+  gdal_calc.py -A $1 --outfile=grass_height.tif --calc="0*(A<101)" --calc="0*(A>103)" --calc="(((2*(A-100)*0.25))-0.25)*100" --type="Int16"
+  gdal_calc.py -A $1 --outfile=shrub_height.tif --calc="0*(A<104)" --calc="0*(A>107)" --calc="250*(A==104)" --calc="750*(A==105)" --calc="2000*(A==106)" --calc="3250*(A==107)" --type="Int16"
+  gdal_calc.py -A $1 --outfile=tree_height.tif --calc="0*(A<108)" --calc="0*(A>111)" --calc="(A==108)*25" --calc="(A==109)*750" --calc="(A==110)*1750" --calc="(A==111)*3750" --type="Int16"
+elif echo $in | grep -q "evh"; then
+  rm -rf grass_perc_cover* shrub_perc_cover* tree_perc_cover*
+  gdal_calc.py -A $1 --outfile=grass_perc_cover.tif --calc="0*(A<121)" --calc="0*(A>129)" --calc="((A-120)*10)+5" --type="Int16"
+  gdal_calc.py -A $1 --outfile=shrub_perc_cover.tif --calc="0*(A<111)" --calc="0*(A>119)" --calc="((A-110)*10)+5" --type="Int16"
+  gdal_calc.py -A $1 --outfile=tree_perc_cover.tif --calc="0*(A<101)" --calc="0*(A>109)" --calc="((A-100)*10)+5" --type="Int16"
+fi
