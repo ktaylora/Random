b1bcf683461552d9bb326a91238e8541f30da95c
Kyle Taylor <ktaylora@users.noreply.github.com>
2016-11-01 11:21:45 -0600

    Update gdal_post-process_landfire.sh

--- gdal_post-process_landfire.sh
+++ gdal_post-process_landfire.sh
@@ -3,7 +3,7 @@
 in=`echo "$1" | awk '{print tolower($0)}'`
 if echo $in | grep -q "evh"; then
   rm -rf grass_height* shrub_height* tree_height*
-  gdal_calc.py -A $1 --outfile=grass_height.tif --calc="0*(A<101)" --calc="0*(A>103)" --calc="(((2*(A-100)*0.25))-0.25)*100" --type="Int16"
+  gdal_calc.py -A $1 --outfile=grass_height.tif --calc="(A<103)*(A>101)*(((2*(A-100)*0.25))-0.25)*100" #25,75,125
   gdal_calc.py -A $1 --outfile=shrub_height.tif --calc="0*(A<104)" --calc="0*(A>107)" --calc="250*(A==104)" --calc="750*(A==105)" --calc="2000*(A==106)" --calc="3250*(A==107)" --type="Int16"
   gdal_calc.py -A $1 --outfile=tree_height.tif --calc="0*(A<108)" --calc="0*(A>111)" --calc="(A==108)*25" --calc="(A==109)*750" --calc="(A==110)*1750" --calc="(A==111)*3750" --type="Int16"
 elif echo $in | grep -q "evh"; then
