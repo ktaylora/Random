c38179eb60fe79d39432fecc6122f464ae6db535
Kyle Taylor <ktaylora@users.noreply.github.com>
2016-11-02 14:36:49 -0600

    Update gdal_post-process_landfire.sh

--- gdal_post-process_landfire.sh
+++ gdal_post-process_landfire.sh
@@ -8,7 +8,7 @@ if echo $in | grep -q "evh"; then
   gdal_calc.py -A $1 --outfile=tree_height.tif --calc="0*(A<108)+(0*(A>111))+((A==108)*25)+((A==109)*750)+((A==110)*1750)+((A==111)*3750)" --type="Int16"
 elif echo $in | grep -q "evh"; then
   rm -rf grass_perc_cover* shrub_perc_cover* tree_perc_cover*
-  gdal_calc.py -A $1 --outfile=grass_perc_cover.tif --calc="(((A>121))+((A<129)))*(((A-120)*10)+5)" --type="Int16"
-  gdal_calc.py -A $1 --outfile=shrub_perc_cover.tif --calc="(((A>111))+((A<119)))*(((A-110)*10)+5)" --type="Int16"
-  gdal_calc.py -A $1 --outfile=tree_perc_cover.tif --calc="(((A>101))+((A<109)))*(((A-100)*10)+5)" --type="Int16"
+  gdal_calc.py -A $1 --outfile=grass_perc_cover.tif --calc="(((A>=121))*((A<=129)))*(((A-120)*10)+5)" --type="Int16"
+  gdal_calc.py -A $1 --outfile=shrub_perc_cover.tif --calc="(((A>=111))*((A<=120)))*(((A-110)*10)+5)" --type="Int16"
+  gdal_calc.py -A $1 --outfile=tree_perc_cover.tif --calc="(((A>=101))*((A<=110)))*(((A-100)*10)+5)" --type="Int16"
 fi
