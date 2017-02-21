47ca54d2ae9b7c347dcc8bb4e39e72ba71af64d8
ktaylora <kyle.a.taylor@gmail.com>
2017-02-21 14:47:33 -0700

    house keeping

--- fetch_and_process_gridded_nass/process.sh
+++ fetch_and_process_gridded_nass/process.sh
@@ -1,15 +1,21 @@
-bash get.sh
+#!/bin/bash
+
+#bash get.sh
 
 for z in `ls *.zip`; do
   7za x $z
 done
 
-rm -rf *.zip
+#rm -rf *.zip
 
 for z in `ls *.img`; do
   echo " -- converting" $z "to GeoTIFF"
   f=`echo $z | awk '{ print substr($1,1,index($1,".")) "tif" }'`
   gdalwarp -of GTIFF $z $f
+  if [ -r $1 ]; then
+    gdalwarp -cutline $1 -crop_to_cutline $f `echo $f | awk '{ print substr($1,1,index($1,".")) "cut.tif" }'` # append with .cut.tif
+    mv `echo $f | awk '{ print substr($1,1,index($1,".")) "cut.tif" }'` $f # over-write original
+  fi
 done
 
 rm -rf *.img *.ige *.rrd *.rde
